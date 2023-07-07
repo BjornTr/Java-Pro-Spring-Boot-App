7fed89d5b67f14950f24b1afe95f62b77e187e92
BjornTr <seryygraf@gmail.com>
2023-07-07 16:30:01 +0300

    minor changes 2 =)

--- src/main/java/com/hillel/springapp/repository/OrderRepository.java
+++ src/main/java/com/hillel/springapp/repository/OrderRepository.java
@@ -16,25 +16,4 @@ import java.util.Optional;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    @NonNull
-    Optional<Order> findById(@NonNull Long id);
-
-    @NonNull <S extends Order> S save(@NonNull S order);
-
-    void delete(@NonNull Order order);
-
-    @Modifying
-    @Query("UPDATE Order o SET o.products = :productList WHERE o.id = :orderId")
-    void addProductToOrder(@Param("orderId") Long orderId, @Param("productList") List<Product> productList);
-
-    @Modifying
-    @Query("UPDATE Order o SET o.products = :productList WHERE o.id = :orderId")
-    void deleteProductFromOrder(@Param("orderId") Long orderId, @Param("productList") List<Product> productList);
-
-
-    @NonNull
-    List<Order> findAll();
-
-    @Query("SELECT o.products FROM Order o WHERE o.id = :orderId")
-    List<Product> findAllProductsInOrder(@Param("orderId") Long orderId);
 }
