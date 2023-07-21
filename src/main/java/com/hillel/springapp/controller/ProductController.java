22db62fa70d161a98c35e15b8be35b12ad552313
BjornTr <seryygraf@gmail.com>
2023-07-21 18:21:02 +0300

    add annotations to block delete and add methods from non adin users

--- src/main/java/com/hillel/springapp/controller/ProductController.java
+++ src/main/java/com/hillel/springapp/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.hillel.springapp.dto.ProductDTO;
 import com.hillel.springapp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,7 +21,7 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProductDTO> getProductById(Long id) {
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable Long id) {
         ProductDTO productDTO = productService.getProductById(id);
         if (productDTO != null) {
             return ResponseEntity.ok(productDTO);
@@ -35,14 +36,16 @@ public class ProductController {
         return ResponseEntity.ok(productDTOs);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
-    public ResponseEntity<ProductDTO> addProduct(ProductDTO productDTO) {
+    public ResponseEntity<ProductDTO> addProduct(@RequestBody ProductDTO productDTO) {
         ProductDTO createdProduct = productService.addProduct(productDTO);
         return ResponseEntity.ok(createdProduct);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduct(Long id) {
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
         boolean deleted = productService.deleteProduct(id);
         if (deleted) {
             return ResponseEntity.noContent().build();
